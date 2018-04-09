db60c7273f274465cdbfe94f5f10256949d6c2ee
Carlos Ferreira <conta@especializati.com.br>
2018-04-09 11:02:40 -0300

    Configs Laravel Socialite

--- config/services.php
+++ config/services.php
@@ -35,4 +35,10 @@ return [
         'secret' => env('STRIPE_SECRET'),
     ],
 
+    'facebook' => [
+        'client_id' => env('FACEBOOK_CLIENT_ID'),         // Your FACEBOOK Client ID
+        'client_secret' => env('FACEBOOK_CLIENT_SECRET'), // Your FACEBOOK Client Secret
+        'redirect' => env('FACEBOOK_URL_REDIRECT'),
+    ],
+
 ];
